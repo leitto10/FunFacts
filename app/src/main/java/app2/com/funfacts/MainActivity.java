99142ac2d383a79b76d97bdbf40ef9ddc707d699
Jose Lopez <leitto10@gmail.com>
2019-06-25 22:19:57 -0700

    adding a button and classes

--- app/src/main/java/app2/com/funfacts/MainActivity.java
+++ app/src/main/java/app2/com/funfacts/MainActivity.java
@@ -1,13 +1,49 @@
 package app2.com.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Random;
+
+import static android.graphics.Color.*;
 
 public class MainActivity extends AppCompatActivity {
+    private FactBook factBook = new FactBook();
+    private ColorWheel colorWheel = new ColorWheel();
+
+    // Declare our views variables
+    private TextView factTextView;
+    private Button showFactButton;
+    private RelativeLayout relativeLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        factTextView = (TextView) findViewById(R.id.factTextView);
+        showFactButton = (Button) findViewById(R.id.showFanFact);
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = factBook.getFact();
+                int color = colorWheel.getColor();
+
+                // update the screen with our new fact
+                factTextView.setText(fact);
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 }
